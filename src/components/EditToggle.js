ab0829e159f75a3aa36ca920df19997bbbadee2a
Aarti Venugopal <venuraghavan@Venus-MacBook-Pro-2.local>
2019-05-07 13:07:51 -0700

    reactminiday2

--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 export default class EditToggle extends Component {
-  render() {
+  render(props) {
     return (
-      <select className="dropDownContainer ml0">
-        <option value="true"> Allow Edit </option>
-        <option value="false"> Disable Edit </option>
-      </select>
+      <select className="dropDownContainer ml0" onChange={ (e) => this.props.update(e.target.value) }>
+      <option value="true"> Allow Edit </option>
+      <option value="false"> Disable Edit </option>
+    </select>
     );
   }
 }
